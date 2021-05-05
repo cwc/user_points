4e1683ca929858d2ffacc6bc44d31b68da9f16d7
Cameron Currie <me@cameroncurrie.net>
2021-05-05 12:57:31 -0500

    Seed 1,000,000 users on DB setup

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias UserPoints.User
+
+# Add 1,000,000 users to the DB
+# insert_all is limited to 65535 params; we need to do 2 params per row, so we'll do 40 batches of 25,000
+Enum.each(0..39, fn _ ->
+  UserPoints.Repo.insert_all(User,
+  for (_ <- 0..24_999) do
+    %{inserted_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
+    updated_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)}
+  end)
+end)
